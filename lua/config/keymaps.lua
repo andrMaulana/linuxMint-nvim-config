f801e3a549285804235f1b89baee1d9f1e2b1566
AndrMaulana <andryymaulana2000@gmail.com>
2024-06-03 22:42:25 +0700

    Lazy configure

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,12 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymaps = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- Increment/decrement
+keymaps.set("n", "+", "<C-a>")
+keymaps.set("n", "-", "<C-x>")
+
+-- Delete a word backwords
+keymaps.set("n", "dw", "vb_d")
